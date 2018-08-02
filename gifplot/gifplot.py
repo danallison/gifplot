c5373a338eda9fa30b24b05fcef6b54cdb55cfe4
Dan Allison <daniel.j.allison@gmail.com>
2018-08-01 21:12:57 -0700

    Refactor

--- gifplot/gifplot.py
+++ gifplot/gifplot.py
@@ -6,22 +6,26 @@ from IPython.display import Image
 
 IMWriter = manimation.writers.avail['imagemagick']
 
+def get_time_map(x, y, t):
+    time_map = defaultdict(lambda: [[],[]])
+    for i, t_i in enumerate(t):
+        time_map[t_i][0].append(x[i])
+        time_map[t_i][1].append(y[i])
+    return time_map
+
 class GifPlot:
     def __init__(self, filename, fps=10, metadata={}):
         self.filename = filename
         self.fps = fps
         self.metadata = metadata
 
-    def animate(self, t, data_fn, viz_step_fn, viz_setup_fn=None):
+    def make_gif(self, t, step, setup=None):
         writer = IMWriter(fps=self.fps, metadata=self.metadata)
-
         fig = plt.figure()
-        viz_step = viz_setup_fn(fig) if viz_setup_fn else None
-
+        prev_step = setup(fig) if setup else None
         with writer.saving(fig, self.filename, 100):
             for t_i in t:
-                data = data_fn(t_i)
-                viz_step = viz_step_fn(t_i, data, viz_step)
+                prev_step = step(t_i, prev_step)
                 writer.grab_frame()
         plt.close(fig)
         try:
@@ -29,22 +33,14 @@ class GifPlot:
         except:
             return fig
 
-    def _get_time_map(self, x, y, t):
-        time_map = defaultdict(lambda: [[],[]])
-        for i, t_i in enumerate(t):
-            time_map[t_i][0].append(x[i])
-            time_map[t_i][1].append(y[i])
-        return time_map
-
     def scatter(self, x, y, t, *args, **kwargs):
-        time_map = self._get_time_map(x, y, t)
-        def data_fn(t_i):
-            return time_map[t_i]
-        def viz_step_fn(t_i, data, viz_step):
-            if viz_step: viz_step.remove()
+        time_map = get_time_map(x, y, t)
+        def step(t_i, prev_step):
+            data = time_map[t_i]
+            if prev_step: prev_step.remove()
             plt.title(f'{t_i}')
             return plt.scatter(data[0], data[1], *args, **kwargs)
-        def viz_setup_fn(fig):
+        def setup(fig):
             plt.xlim(np.min(x), np.max(x))
             plt.ylim(np.min(y), np.max(y))
-        return self.animate(sorted(list(time_map.keys())), data_fn, viz_step_fn, viz_setup_fn)
+        return self.make_gif(sorted(list(time_map.keys())), step, setup)
